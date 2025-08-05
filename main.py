3417aa23bf928559d8cc2ee408a15b4e75074b9f
Vmex26 <Hanxdlol2602@gmail.com>
2025-08-05 00:35:56 -0500

    Primer commit: Agregando funciones (que aun no hacen nada)

---
+++ main.py
@@ -0,0 +1,10 @@
+#Aqui va estar todo el codigo del programa 
+
+from datetime import datetime
+
+#Esta funcion solo sera llamada si es que el usuario habre por primera ves el programa
+#o por si no hay un archivo de configuracion donde esten alojados los datos
+def pedir_birth():
+    # Aqui implementare la logica donde le pedire al usuario su fecha de cumpleaños
+    pass
+
