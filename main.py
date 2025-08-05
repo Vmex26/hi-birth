a7efb3515e61243fca53acffd19e4d44edecbcdd
Vmex26 <Hanxdlol2602@gmail.com>
2025-08-05 13:28:54 -0500

    Funcion pedir_birth()

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ from datetime import datetime
 
 #Esta funcion solo sera llamada si es que el usuario habre por primera ves el programa
 #o por si no hay un archivo de configuracion donde esten alojados los datos
+
 def pedir_birth():
     # Aqui implementare la logica donde le pedire al usuario su fecha de cumpleaños
-    pass
+    while True:
+        fecha = input("Introduce tu fecha de nacimiento DD/MM/YY (ejemplo: 31/12/2015): ")
+        try:
+            datetime.strptime(fecha, '%d/%m/%Y')
+            break
+        except ValueError:
+            print("Formato no reconocido, por favor intentalo denuevo")
 
+pedir_birth() #Con esto pruebo que la funcion este correcta
