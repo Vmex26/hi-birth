e042201c7723b16a4cc16e8daf6a09f68ba16da2
Vmex26 <hanxdlol2602@gmail.com>
2026-01-03 19:59:39 -0500

    changing the code

--- main.py
+++ main.py
@@ -4,48 +4,44 @@ import os
 import getpass
 from datetime import datetime
 import json
-from utils.colors import GREEN, RED, END, QST, FATAL #Se llama a las utilidades mas para decorar que funcionalidad
 
 #Esta funcion solo sera llamada si es que el usuario habre por primera ves el programa
 #o por si no hay un archivo de configuracion donde esten alojados los datos
 
+USERNAME = getpass.getuser()
+
 def first_init(): #Esta funcion confirmara si es la primera ves que se ejecuta o no
     #si es la primera ves que se ejecuta llamara la funcion create_json para crear el archivo
     #donde este guardado el username y la fecha
-    username = getpass.getuser()
-    archive_path = os.path.join(os.path.expanduser("~"), ".config", "hibirth", f"{username}_data.json")
+    archive_path = os.path.join(os.path.expanduser("~"), ".config", "hibirth", f"{USERNAME}_data.json")
     
     if not os.path.exists(archive_path):
         create_json()
     else:
         dias_restantes = calcular_dias_restantes() #Aqui llamare a una funcion que se encarge de calcular los dias que faltan
         
-        #Daremos diferentes prints dependiendo de cuantos dias faltan
-        if dias_restantes >= 20:
-            print(f"Quedan {dias_restantes} dias para tu cumpleaños {username}, ¡Se paciente!")
-        elif dias_restantes < 10 and dias_restantes > 1:
-            print(f"Quedan {dias_restantes} dias para tu cumpleaños ya no falta mucho {username}!")
-        elif dias_restantes == 1:
-            print(GREEN + "Mañana es tu cumpleaños!" + END + f"espero que tengas todo preparado para entonces {username}")
-        elif dias_restantes == 0:
-            print(GREEN + "FELIZ CUMPLEAÑOS" + END + f"{username} disfruta del dia!")
-        elif dias_restantes == 359:
-            print(f"Que tal te fue en tu cumpleaños de ayer {username}?")
+        #Print por si es el cumpleaños
+        if dias_restantes == 0:
+            print("FELIZ CUMPLEAÑOS" + f"{USERNAME} disfruta del dia!")
+
+        else:
+            print("faltan" + f"{dias_restantes}")
+
 
 def pedir_birth():
     # Aqui implementare la logica donde le pedire al usuario su fecha de cumpleaños
     while True:
         #Se le pide al usuario el birthday
-        fecha = input(QST + " Introduce tu fecha de nacimiento DD/MM/YYYY (ejemplo: 31/12/2015): ") 
+        fecha = input(" Introduce tu fecha de nacimiento DD/MM/YYYY (ejemplo: 31/12/2015): ") 
     
         try:
             #Comprobamos que la fecha este en el formato indicado
             birth = datetime.strptime(fecha, '%d/%m/%Y')
             if birth > datetime.today():
-                print(FATAL+RED+"No puedes tener edad negativa o igual a 0\n"+END)
+                print("No puedes tener edad negativa o igual a 0\n")
                 continue
             else:
-                confirm = input(QST+ f" Entonces la fecha es {fecha} correcto? (Y/N)") 
+                confirm = input(f" Entonces la fecha es {fecha} correcto? (Y/N)") 
                 
                 if confirm == "y" or confirm == "Y":
                     return fecha
@@ -54,19 +50,18 @@ def pedir_birth():
         
         except ValueError:
             
-            print(FATAL + " Formato no reconocido, por favor intentalo denuevo\n")
+            print(" Formato no reconocido, por favor intentalo denuevo\n")
 
 
 def create_json(): 
     #Esta funcion creara el json donde estara guardado el nombre y la fecha 
-    username = getpass.getuser() #conseguimos el nombre de usuario
     
     fecha = pedir_birth() #llamamos a la funcion pedir_birth
          
     
     datos = {
-            "name" : f"{username}",
-            "fecha" : f"{fecha}", 
+            "fecha" : f"{fecha}",
+            "create_at" : f"{datetime.today}"
     } #creamos un diccionario con los datos
     
     
@@ -75,7 +70,7 @@ def create_json():
     os.makedirs(config_dir, exist_ok=True)  # Crea la carpeta si no existe
     
     # Ruta del archivo donde esten los datos
-    archive_path = os.path.join(config_dir, f"{username}_data.json") #se nombra el archivo con el nombre de usuario
+    archive_path = os.path.join(config_dir, f"{USERNAME}_data.json") #se nombra el archivo con el nombre de usuario
     #Ejemplo = home/user/.config/hibirth/user_data.json
     
     with open(archive_path, "w") as archive: #Abrimos el archivo (o lo crea), y lo nombramos archive
@@ -84,8 +79,7 @@ def create_json():
 
 def calcular_dias_restantes():
     #calculamos los dias que faltan para el proximo cumpleaños
-    username = getpass.getuser()
-    archive_path = os.path.join(os.path.expanduser("~"), ".config", "hibirth", f"{username}_data.json")
+    archive_path = os.path.join(os.path.expanduser("~"), ".config", "hibirth", f"{USERNAME}_data.json")
     
     with open(archive_path, "r") as archive: #Abrimos el archivo
         datos = json.load(archive) #cargamos los datos en formato json
@@ -122,7 +116,7 @@ if __name__ == "__main__":
     try:    
         first_init() #Ejecutamos el script
     except KeyboardInterrupt:
-        print("\n" + FATAL + RED + " Saliendo..." + END)
+        print("\n" + " Saliendo...")
         exit(1)
 
 
